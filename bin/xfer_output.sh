43440e49dda93473f79749c880527d0bdded276e
John Evans <john.g.evans@noaa.gov>
2018-12-14 08:04:17 -0500

    Update for new workstation
    nco-lw7-jevans3 has different mounts, so the rsync commands are
    different.

--- bin/xfer_output.sh
+++ bin/xfer_output.sh
@@ -2,6 +2,10 @@
 
 # Transfer the summary graphics for the web logs to the intranet.
 
+set -x
 project=${1}
 
-rsync -avz "$project" -e ssh jevans@nco:/mnt/intra_wwwdev/ncep/ncepintradev/htdocs/ncep_common/nowcoast/analytics
+# rsync -avz "$project" -e ssh jevans@nco:/mnt/intra_wwwdev/ncep/ncepintradev/htdocs/ncep_common/nowcoast/analytics
+
+remote_root=/usr2/ncep/ncepintradev/htdocs/ncep_common/nowcoast/analytics/"$project"
+rsync -avz $project -e ssh jevans@vm-lnx-wwwdev1:"$remote_root"
--- bin/xfer_webalizer.sh
+++ bin/xfer_webalizer.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+# rsync -avz webalizer -e ssh jevans@nco:/mnt/intra_wwwdev/ncep/ncepintradev/htdocs/ncep_common/nowcoast/
+
 cd $HOME/www
-rsync -avz webalizer -e ssh jevans@nco:/mnt/intra_wwwdev/ncep/ncepintradev/htdocs/ncep_common/nowcoast/
+remote_root=/usr2/ncep/ncepintradev/htdocs/ncep_common/nowcoast
+rsync -avz webalizer -e ssh jevans@vm-lnx-wwwdev1:"$remote_root"
