3b500a2244bdc24a6ed7e5ada6ae34111455b71a
John Evans <john.g.evans.ne@gmail.com>
2020-03-18 12:59:19 -0700

    Refactor referer graphics
    Replace pandas with SQL where possible.  This actually has reduced the
    total time from about 6 seconds to 1 second.

--- log_processing/arcgis_apache_logs/referer.py
+++ log_processing/arcgis_apache_logs/referer.py
@@ -4,6 +4,7 @@ import importlib.resources as ir
 import urllib.parse
 
 # 3rd party library imports
+import datetime as dt
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -38,15 +39,6 @@ class RefererProcessor(CommonProcessor):
             All referers that have been previously encountered.
         """
         super().__init__(**kwargs)
-
-        self.time_series_sql = f"""
-            SELECT a.date, SUM(a.hits) as hits, SUM(a.errors) as errors,
-                   SUM(a.nbytes) as nbytes, b.name as referer
-            FROM referer_logs a INNER JOIN referer_lut b
-            ON a.id = b.id
-            GROUP BY a.date, referer
-            ORDER BY a.date
-            """
 
         self.data_retention_days = 7
 
@@ -144,36 +136,60 @@ class RefererProcessor(CommonProcessor):
         html_doc : lxml.etree.ElementTree
             HTML document for the logs.
         """
-        self.get_timeseries()
-        self.summarize_referers(html_doc)
-        self.summarize_transactions(html_doc)
-        self.summarize_bandwidth(html_doc)
+        self.logger.info(f'Referers:  starting graphics...')
+        self.logger.info(f'Referers:  getting initial time series...')
+
+        self.logger.info(f'Referers:  summarizing table ...')
+        self.summarize_referer_table(html_doc)
+
+        top_referers = self.get_top_referers()
+
+        self.logger.info(f'Referers:  summarizing transactions ...')
+        self.summarize_transactions(top_referers, html_doc)
+        self.logger.info(f'Referers:  summarizing bandwidth ...')
+        self.summarize_bandwidth(top_referers, html_doc)
+
+        self.logger.info(f'Referers:  done with graphics...')
 
     def get_top_referers(self):
         # who are the top referers for today?
-        df = self.df_today.copy()
+        sql = r"""
+            SELECT
+                SUM(logs.hits) as hits,
+                lut.name as referer
+            FROM referer_logs logs INNER JOIN referer_lut lut using(id)
+            where logs.date = '{yesterday}'
+            GROUP BY referer
+            order by hits desc
+            limit 7
+            """
+        yesterday = (dt.date.today() - dt.timedelta(days=1)).isoformat()
+        df = pd.read_sql(sql, self.conn, index_col='referer')
 
-        df['valid_hits'] = df['hits'] - df['errors']
-        top_referers = (df.groupby('referer')
-                          .sum()
-                          .sort_values(by='valid_hits', ascending=False)
-                          .head(n=7)
-                          .index)
+        return df.index
 
-        return top_referers
-
-    def summarize_bandwidth(self, html_doc):
+    def summarize_bandwidth(self, top_referers, html_doc):
         """
         Create a PNG showing the top referers (bytes) over the last few days.
         """
-        top_referers = self.get_top_referers()
 
         # Now restrict the hourly data over the last few days to those
         # referers.  Then restrict to valid hits.  And rename valid_hits to
         # hits.
-        df = self.df[self.df.referer.isin(top_referers)].sort_values(by='date')
-        df = df[['date', 'referer', 'nbytes']]
-        df['nbytes'] /= (1024 ** 3)
+        query = """
+            SELECT
+                logs.date,
+                logs.nbytes::real / 1024 ^ 3 as nbytes,
+                lut.name as referer
+            FROM referer_logs logs INNER JOIN referer_lut lut using(id)
+            where
+                date > '{start_time}'
+                and lut.name in {top_referers}
+            order by logs.date desc
+        """
+        query = query.format(top_referers=tuple(top_referers),
+                             start_time=dt.date.today()-dt.timedelta(days=14))
+        df = pd.read_sql(query, self.conn)
 
         df = df.pivot(index='date', columns='referer', values='nbytes')
 
@@ -190,24 +206,27 @@ class RefererProcessor(CommonProcessor):
         }
         self.write_html_and_image_output(df, html_doc, **kwargs)
 
-    def summarize_transactions(self, html_doc):
+    def summarize_transactions(self, top_referers, html_doc):
         """
         Create a PNG showing the top referers over the last few days.
         """
-        top_referers = self.get_top_referers()
+        query = """
+            SELECT
+                logs.date,
+                logs.hits::real / 3600 as hits,
+                lut.name as referer
+            FROM referer_logs logs INNER JOIN referer_lut lut using(id)
+            where
+                date > '{start_time}'
+                and lut.name in {top_referers}
+            order by logs.date desc
+        """
+        query = query.format(top_referers=tuple(top_referers),
+                             start_time=dt.date.today()-dt.timedelta(days=14))
+        df = pd.read_sql(query, self.conn)
 
-        df = self.df.copy()
-
-        # Now restrict the hourly data over the last few days to those
-        # referers.  Then restrict to valid hits.  And rename valid_hits to
-        # hits.
-        df = df[df.referer.isin(top_referers)].sort_values(by='date').copy()
-        df['hits'] = df['hits'] - df['errors']
-        df = df[['date', 'referer', 'hits']]
-
-        # Rescale them from hits/hour to hits/second
-        df['hits'] /= 3600
-
+        # Pivot the referers into the columns so that we can plot them as
+        # time series.
         df = df.pivot(index='date', columns='referer', values='hits')
 
         # Order them by max value.
@@ -225,7 +244,7 @@ class RefererProcessor(CommonProcessor):
         }
         self.write_html_and_image_output(df, html_doc, **kwargs)
 
-    def summarize_referers(self, html_doc):
+    def summarize_referer_table(self, html_doc):
         """
         Calculate
 
@@ -235,7 +254,20 @@ class RefererProcessor(CommonProcessor):
 
         Just for the latest day, though.
         """
-        df = self.df_today.copy().groupby('referer').sum()
+        sql = r"""
+            SELECT
+                SUM(logs.hits) as hits,
+                SUM(logs.errors) as errors,
+                SUM(logs.nbytes) as nbytes,
+                lut.name as referer
+            FROM referer_logs logs INNER JOIN referer_lut lut using(id)
+            where logs.date = '{yesterday}'
+            GROUP BY referer
+            order by hits desc
+            """
+        yesterday = (dt.date.today() - dt.timedelta(days=1)).isoformat()
+        sql = sql.format(yesterday=yesterday)
+        df = pd.read_sql(sql, self.conn)
 
         total_hits = df['hits'].sum()
         total_bytes = df['nbytes'].sum()
@@ -245,10 +277,6 @@ class RefererProcessor(CommonProcessor):
         df['hits %'] = df['hits'] / total_hits * 100
         df['GBytes'] = df['nbytes'] / (1024 ** 3)  # GBytes
         df['GBytes %'] = df['nbytes'] / total_bytes * 100
-
-        idx = df['errors'].isnull()
-        df.loc[idx, ('errors')] = 0
-        df['errors'] = df['errors'].astype(np.uint64)
 
         df['errors: % of all hits'] = df['errors'] / total_hits * 100
         df['errors: % of all errors'] = df['errors'] / total_errors * 100
@@ -264,9 +292,11 @@ class RefererProcessor(CommonProcessor):
             'errors: % of all errors'
         ]
         df = df[reordered_cols]
-        df = df.sort_values(by='hits', ascending=False).head(15)
 
-        yesterday = (dt.date.today() - dt.timedelta(days=1)).isoformat()
+        # Limit our display to the top 15.  Do this here to get all the summary
+        # statistics first.
+        df = df.head(15)
+
         kwargs = {
             'aname': 'referers',
             'atext': 'Top Referers',
